126c04fc8f3fa77526bf77176c19cbc250f59c81
trgy2145 <trgy2145@gmail.com>
2022-02-23 20:04:44 +0100

    first commit

---
+++ todo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=50)
+    PRIORITY = (
+        ("1", "High"),
+        ("2", "Medium"),
+        ("3", "Low"),
+    )
+    priority = models.CharField(max_length=50, choices=PRIORITY)
+    isdone = models.BooleanField(default=False)
+    updated_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
